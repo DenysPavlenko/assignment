9a11f0b046f7df68abf80d9fb57a8663c87a8b57
Denys Pavlenko <de.pavlenko@gmail.com>
2021-04-30 18:00:44 +0300

    Create contact-card component

---
+++ src/components/contact-card/index.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { CSSTransition } from 'react-transition-group';
+// Components
+import Typography from 'components/typography';
+// Hooks
+import useClickOutside from 'hooks/use-click-outside';
+// Styles
+import './index.sass';
+// Assets
+import { ReactComponent as CloseIcon } from 'assets/images/icons/close.svg';
+
+const ContactCard = ({ children, avatar, firstName, lastName, className, ...otherProps }) => {
+  const [isOpened, setIsOpened] = React.useState(false);
+  const contactCardRef = React.useRef(null);
+  const contactCardWrapRef = React.useRef(null);
+
+  const classes = classNames({
+    'contact-card': true,
+    [className]: className
+  });
+
+  const handleOpen = () => setIsOpened(true);
+  const handleClose = () => setIsOpened(false);
+
+  useClickOutside(contactCardRef, handleClose);
+
+  return (
+    <div ref={contactCardRef}>
+      {React.Children.map(children, child => (
+        React.cloneElement(child, { onClick: handleOpen })
+      ))}
+      <div className={classes}>
+        <CSSTransition nodeRef={contactCardWrapRef} in={isOpened} timeout={300} classNames="contact-card-animation" unmountOnExit>
+          <div className="contact-card__wrap" ref={contactCardWrapRef}>
+            <CloseIcon className="contact-card__close" onClick={handleClose} />
+            <div className="contact-card__row">
+              <div className="contact-card__left">
+                <img className="contact-card__avatar" src={avatar} alt="avatar" />
+              </div>
+              <div className="contact-card__right">
+                <Typography component="h5" className="contact-card__name">{firstName} {lastName}</Typography>
+                <div>
+                </div>
+                <table className="contact-card__details">
+                  <tbody>
+                    {Object.keys(otherProps).map(key => (
+                      <tr key={key}>
+                        <td className="contact-card__details-name">
+                          <Typography component="span" variant="h6">{key}:</Typography>
+                        </td>
+                        <td className="contact-card__details-value">
+                          <Typography component="span" variant="p">{otherProps[key]}</Typography>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </CSSTransition>
+      </div>
+    </div>
+  );
+};
+
+ContactCard.propTypes = {
+  children: PropTypes.node,
+  avatar: PropTypes.string,
+  firstName: PropTypes.string,
+  lastName: PropTypes.string,
+  className: PropTypes.string,
+};
+
+export default ContactCard;
