084badd0f783afd6f0b180fea00a483cd0b0d63c
Denys Pavlenko <de.pavlenko@gmail.com>
2021-04-30 12:25:59 +0300

    Create button component

---
+++ src/components/button/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+// Styles
+import './styles.sass';
+
+const Button = ({ children, href, className, variant, disabled, onClick, type, style }) => {
+  const classes = classNames({
+    'button': variant && !disabled,
+    [`button--${variant}`]: variant,
+    [className]: className
+  });
+
+  const Tag = href ? 'a' : 'button';
+
+  return (
+    <Tag href={href} className={classes} disabled={!href && disabled} onClick={onClick} style={style} type={type}>
+      {children}
+    </Tag>
+  );
+};
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+  href: PropTypes.string,
+  className: PropTypes.string,
+  variant: PropTypes.string,
+  disabled: PropTypes.bool,
+  onClick: PropTypes.func,
+  type: PropTypes.string,
+  style: PropTypes.object,
+};
+
+export default Button;
