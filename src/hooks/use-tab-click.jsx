4153d3cf50dbc5b1ef5c4277532a56d64242a0f4
Denys Pavlenko <de.pavlenko@gmail.com>
2021-04-30 18:58:15 +0300

    Create use-tab-click hook

---
+++ src/hooks/use-tab-click.jsx
@@ -0,0 +1,16 @@
+import { useEffect } from 'react';
+
+const useTabClick = (ref, handler) => {
+  useEffect(() => {
+    const listener = e => {
+      var code = e.keyCode || e.which;
+      if (code === 9) {
+        handler(e);
+      }
+    };
+    document.addEventListener('keydown', listener);
+    return () => document.removeEventListener('keydown', listener);
+  }, [ref, handler]);
+};
+
+export default useTabClick;
