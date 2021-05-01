2a126ed67afa90bbe45965bde3d77691e291695a
Denys Pavlenko <de.pavlenko@gmail.com>
2021-05-01 16:25:15 +0300

    Implement typography tests

---
+++ src/components/typography/index.test.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import { shallow } from 'enzyme';
+import Typography from './index';
+import { checkProps } from 'test-utils/index';
+
+const defaultProps = {
+  children: <span></span>,
+  component: 'p',
+  variant: 'p',
+  className: undefined,
+};
+
+const setup = (props = {}) => {
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<Typography {...setupProps} />);
+};
+
+describe('Typography component', () => {
+  test('renders without error', () => {
+    let wrapper = setup();
+    const component = wrapper.find('p');
+    expect(component.length).toBe(1);
+  });
+  test('does not add an empty class', () => {
+    let wrapper = setup({ variant: undefined });
+    const component = wrapper.find('p');
+    expect(component.get(0).props.className).toBe(null);
+  });
+  test('does not throw warning with expected props', () => {
+    const expectedProps = { ...defaultProps, className: 'test' };
+    const propsError = checkProps(Typography, expectedProps);
+    expect(propsError).toBeUndefined();
+  });
+});
