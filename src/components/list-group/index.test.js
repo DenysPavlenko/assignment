73a829b858542bda79bb6b509a8bcad8d1207af7
Denys Pavlenko <de.pavlenko@gmail.com>
2021-05-01 14:39:04 +0300

    Implement list-group tests

---
+++ src/components/list-group/index.test.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import { shallow } from 'enzyme';
+import ListGroup from './index';
+import { checkProps } from 'test-utils/index';
+
+const defaultProps = {
+  children: ['<div className="list-group__col"></div>'],
+  className: 'test',
+};
+
+const setup = (props = {}) => {
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<ListGroup {...setupProps} />);
+};
+
+describe('ListGroup component', () => {
+  test('renders without error', () => {
+    let wrapper = setup();
+    const component = wrapper.find('.list-group');
+    expect(component.length).toBe(1);
+  });
+  test('does not throw warning with expected props', () => {
+    const expectedProps = { ...defaultProps };
+    const propsError = checkProps(ListGroup, expectedProps);
+    expect(propsError).toBeUndefined();
+  });
+});
