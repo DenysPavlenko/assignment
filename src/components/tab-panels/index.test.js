fa2257fc1da7d1444147ad86801927c8ee5b0984
Denys Pavlenko <de.pavlenko@gmail.com>
2021-05-01 16:19:08 +0300

    Implement tab-panels tests

---
+++ src/components/tab-panels/index.test.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import { shallow } from 'enzyme';
+import TabPanels from './index';
+import { checkProps } from 'test-utils/index';
+
+const defaultProps = {
+  children: <span></span>,
+  activePanel: 'a',
+  className: 'test',
+};
+
+const setup = (props = {}) => {
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<TabPanels {...setupProps} />);
+};
+
+describe('TabPanels component', () => {
+  test('renders without error', () => {
+    let wrapper = setup();
+    const component = wrapper.find('.tab-panels');
+    expect(component.length).toBe(1);
+  });
+  test('does not throw warning with expected props', () => {
+    const expectedProps = { ...defaultProps, onClick: () => { } };
+    const propsError = checkProps(TabPanels, expectedProps);
+    expect(propsError).toBeUndefined();
+  });
+});
