48c611c3d23e8d712c063e5a463b8709c701ecec
Denys Pavlenko <de.pavlenko@gmail.com>
2021-05-01 12:52:29 +0300

    Rename button style.sass to index.sass

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 // Styles
-import './styles.sass';
+import './index.sass';
 
 const Button = ({ children, href, className, variant, disabled, onClick, type, style }) => {
   const classes = classNames({
