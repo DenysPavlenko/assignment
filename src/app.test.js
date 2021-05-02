e7388154a954bfe512f1a8b2b412a9e35e6eb2bd
Denys Pavlenko <de.pavlenko@gmail.com>
2021-05-02 18:40:19 +0300

    Implement app tests

---
+++ src/app.test.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+
+const setup = () => {
+  return shallow(<App />);
+};
+
+test('App renders wothout error', () => {
+  const wrapper = setup();
+  const compoent = wrapper.find('.app');
+  expect(compoent.length).toBe(1);
+});
